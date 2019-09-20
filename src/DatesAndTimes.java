e3e8bc6801c5c77989c000989e58b24f45c61254
Esther <esther191299@gmail.com>
2019-09-20 21:12:59 +0200

    src

---
+++ src/DatesAndTimes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class DatesAndTimes {
+	public static void main(String[] args) {
+		
+		Calendar now = Calendar.getInstance();
+		
+		now.setTimeInMillis(0);
+		System.out.println(now.getTimeInMillis());
+		System.out.println(now.get(Calendar.MONTH)+ 1 );
+		System.out.println(now.get(Calendar.DAY_OF_MONTH));
+		System.out.println(now.get(Calendar.YEAR));
+	}
+
+}
